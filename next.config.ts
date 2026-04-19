693f3dd2f61bf421dd53878e5c573b8481d0440c
Snowhq <nuhuibrahimnuhu91@gmail.com>
2026-04-19 14:36:28 +0100

    Add .locusbuild for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: [
-    "@solana-program/token-2022",
-    "@solana-program/token",
-    "@coinbase/wallet-sdk",
-    "@base-org/account",
-  ],
+  transpilePackages: ["@solana-program/token-2022", "@solana-program/token", "@coinbase/wallet-sdk", "@base-org/account"],
 };
 
 export default nextConfig;
